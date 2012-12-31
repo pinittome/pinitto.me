04ec14efb34ff323f7701543645dcce87c725c22
Steven Lloyd Watkin <lloyd@evilprofessor.co.uk>
2012-12-31 15:04:24 +0000

    Added some production settings for socket.io

--- src/io.js
+++ src/io.js
@@ -26,6 +26,22 @@ io.configure(function (){
 	});
 });
 
+if (environment == 'production') {
+	io.configure('production', function(){
+    io.enable('browser client minification');  // send minified client
+    io.enable('browser client etag');          // apply etag caching logic based on version number
+    io.enable('browser client gzip');          // gzip the file
+    io.set('log level', 1);                    // reduce logging
+    io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+           'websocket'
+         , 'flashsocket'
+         , 'htmlfile'
+         , 'xhr-polling'
+         , 'jsonp-polling'
+        ]);
+    });
+}
+
 io.sockets.on('connection', function (socket) {
 
     user  = require('./classes/user');
