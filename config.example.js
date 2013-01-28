dd5416862f3b48e6bd95a388146c6e9b4ef13178
Steven Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-01-28 22:49:38 +0000

    Include example config file

---
+++ config.example.js
@@ -0,0 +1,23 @@
+module.exports = {
+    version: '0.1.0',
+
+    database: {
+        name: 'test',
+        host: '127.0.0.1',
+        port: 27017
+    },
+    /* Socket.io transports to use - comment for all excluding flashsocket */
+    transports: [],
+    
+    app: {
+	    twitter: "pinittome", 
+	    analytics: {
+	        google: 'UA-XXXX-XX' 
+	    },
+            limits: {
+                card: {
+                    wait: 0.5
+                }
+            }
+    }
+};
