4340aa660464f766f3f74b97172f3e4b0673d56c
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-02-13 17:04:48 +0000

    On reconnect, attempt to rejoin the board

--- public/js/board/connection.js
+++ public/js/board/connection.js
@@ -1,7 +1,7 @@
 define(['socket', 'user'], function(socket, user) {
     socket.on('connect', function(data) {
         console.log("Requesting to join board: " + boardId)
-        socket.emit('board.join', {id: boardId, user: user.name} );
+        socket.emit('board.join', {id: boardId, user: user.name});
         if (user.name) user.hasSetName = true;
         user.id = socket.socket.sessionid;
     });
@@ -9,5 +9,10 @@ define(['socket', 'user'], function(socket, user) {
     socket.on('connect.fail', function(reason) {
         console.log("Not authorised to see this board. Redirect to login.");
         document.location.href = "/login/" + boardId;
+    });
+    
+    socket.on('reconnect', function() {
+    	socket.emit('board.join', {id: boardId, user: user.name});
+    	user.id = socket.socket.sessionid;
     });
 })
