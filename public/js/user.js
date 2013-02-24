4fa786385985cc136a4bc3dfadd4ee74def98e81
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-02-24 14:53:20 +0000

    Remove user entries already in place. Only attempt to join board a reduced number of times

--- public/js/user.js
+++ public/js/user.js
@@ -15,8 +15,8 @@ define(['jquery', 'socket', 'util/notification', 'util/update-user-count', 'boot
     }
     User.prototype.addToList = function(data) {
         isUser = (user.id == data.userId) ? ' (me)' : '';
+        $('#user-' + data.userId).remove();
         newUser = document.createElement('li');
-        $('id', 'user-' + data.userId).remove();
         $(newUser).attr('id', 'user-' + data.userId).append(
             '<a href="#"><i class="icon-user"></i> <span>' 
                 + data.name + '</span>' + isUser + '</a>'
