50b6d6b146d4ee191bf8d37f7bf3b6f9be70d2ad
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-02-27 09:08:33 +0000

    Redirect users to where they wanted to go after authentication redirect

--- src/routes/login.js
+++ src/routes/login.js
@@ -43,18 +43,24 @@ exports.post = function(req, res) {
         	req.session.access = a.getLevel(board, hash);
         	req.session.board  = id;
         	var url = '/' + id
-	       // if (req.param('referrer')
-	        //    && (req.param('referrer').split('/')[2] == req.headers.host)) {
-	        //    url = req.param('referrer')	
-	       // }
+	        if (req.param('referrer')
+	            && (req.param('referrer').split('/')[2] == req.headers.host)) {
+	            url = req.param('referrer')	
+	        }
 	        if (req.session.access != a.NONE) {
 	        	console.log('User authenticated, sending them to ' + url)
 	            res.redirect(url);
 	            return;
 	        }
 	        console.log("Password incorrect")
-	        options.errors = { 'error': 'Incorrect password for this board' }
-	        res.redirect('/login/' + id)
+	        options          = cloneextend.clone(config.project)
+            options.app      = config.app
+            options.pageName = 'Authorisation for board access'
+            options.totals   = totals
+            options.boardId  = id
+	        options.referrer = req.param('referrer')
+	        options.errors   = { 'error': 'Incorrect password for this board' }
+	        res.render('login', options)
         })
     });
 }
