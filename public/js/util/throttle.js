23978edeeab7340f63fad8ebc7ff76790df28968
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-05-11 20:03:12 +0100

    Add missing JS file!

---
+++ public/js/util/throttle.js
@@ -0,0 +1,24 @@
+define([], function() {
+    // From Remi Sharp
+    // http://remysharp.com/2010/07/21/throttling-function-calls/
+    return function throttle(fn, threshhold, scope) {
+        threshhold || (threshhold = 250)
+        var last, deferTimer
+        return function() {
+            var context = scope || this
+            var now = +new Date,
+                args = arguments;
+            if (last && now < last + threshhold) {
+                // hold on to it
+                clearTimeout(deferTimer)
+                deferTimer = setTimeout(function () {
+                    last = now
+                    fn.apply(context, args)
+                }, threshhold)
+            } else {
+                last = now
+                fn.apply(context, args)
+            }
+        }
+    }
+})
