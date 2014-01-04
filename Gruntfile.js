975697850b9928116f1adf60e3cd7d54db824846
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-01-04 16:19:52 +0000

    Support syntax checking

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            allFiles: ['Gruntfile.js', 'public/js/**/*.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        mochacli: {
+            all: ['test/**/*.js'],
+            options: {
+                reporter: 'spec',
+                ui: 'tdd'
+            }
+        }
+    })
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint')
+    grunt.loadNpmTasks('grunt-mocha-cli')
+
+    // Configure tasks
+    grunt.registerTask('default', ['test'])
+    //grunt.registerTask('test', ['mochacli', 'jshint'])
+    grunt.registerTask('test', ['mochacli'])
+}
