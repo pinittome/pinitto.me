b147d3631110eeffcf2962ea5d00f8230f0eb77a
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-01-13 14:57:25 +0000

    Extra colours

--- public/js/util/determine-css-class.js
+++ public/js/util/determine-css-class.js
@@ -1,14 +1,18 @@
 define(function() {
     return function(card) {
-        validCssClasses = [ 'yellow', 'green', 'red', 'blue', 'white']
+        validCssClasses = [ 
+            'yellow', 'light-green', 'purple', 'grey', 'pink',
+            'green', 'orange', 'blue', 'red', 'lavender', 
+            'buff', 'mint', 'white'
+        ]
         for (var index = 0; index < validCssClasses.length; index++) {
             if (card.is('.card-' + validCssClasses[index])) {
-                if (index == validCssClasses.length - 1) {
-                    index = -1;
+                if (index === validCssClasses.length - 1) {
+                    index = -1
                 }
-                return validCssClasses[index+1];
+                return validCssClasses[index+1]
             }
         }
-        return validCssClasses[0];         
+        return validCssClasses[0]         
     }
-});
+})
