47628601487e579ebaaad3cf166c7065d54fc8c0
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-05-31 21:08:21 +0100

    Add massah

---
+++ test/helper.js
@@ -0,0 +1,15 @@
+var startApplication = function(done) {
+    done()
+}
+
+var stopApplication = function(done) {
+    done()
+}
+
+var beforeScenario = function(annotations, context) {}
+
+module.exports = {
+    startApplication: startApplication,
+    stopApplication: stopApplication,
+    beforeScenario: beforeScenario
+}
