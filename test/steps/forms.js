e0f464f518765d28b95ab32563c5af7b9bef936c
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-08-30 20:53:51 +0100

    Clear form fields before entering new values

--- test/steps/forms.js
+++ test/steps/forms.js
@@ -3,7 +3,8 @@ var helper = require('massah/helper')
 module.exports = (function() {
     var library = helper.getLibrary()
         .given('I enter \'(.*)\' in the \'(.*)\' field', function(value, field) {
-            this.driver.input('*[name="' + field + '"]').enter(value)    
+            this.driver.input('*[name="' + field + '"]').clear()
+            this.driver.input('*[name="' + field + '"]').enter(value)
             if (!this.params.fields) this.params.fields = {}
             this.params.fields[field] = value
         })
