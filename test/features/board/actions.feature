6390e2363b1d6a1a85d95687cfb86c0bd03805dc
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-09-14 14:15:08 +0100

    Add test for board title

--- test/features/board/actions.feature
+++ test/features/board/actions.feature
@@ -7,3 +7,9 @@ Background:
 Scenario: When creating a board the name popup appears
 
     Then an success notification of 'Hey! Did you know you can set your name by clicking "set name" in the settings menu, or click here... quick!' should be seen
+
+@Only
+Scenario: Can change the board title
+
+    When I change the board title to 'This is my board'
+    Then the board title is updated 
--- test/steps/pages/board.js
+++ test/steps/pages/board.js
@@ -3,6 +3,35 @@ var massahHelper = require('massah/helper')
 
 module.exports = (function() {
     var library = massahHelper.getLibrary()
+        .when('I change the board title to \'(.*)\'', function(title) {
+            var driver = this.driver
+            var params = this.params
+            driver.wait(function() {
+                return driver.element('div.modal-backdrop').then(
+                    function() { return false },
+                    function() { return true }
+                )
+            }, 15000, 'Waiting for connection modal to close')
+
+            driver.element('a[title="Settings"]').click()
+            driver.element('a.open-set-board-name-modal').click()
+            driver.wait(function() {
+                return driver.element('input.name-set').then(
+                    function() {
+                        driver.element('input.name-set').enter(title)
+                        driver.element('#update-board-name').click()
+                        params.boardTitle = title
+                        return true 
+                    },
+                    function() { return false }
+                )
+            }, 5000, 'Waiting for name modal to open')
+        })
+        .then('the board title is updated', function() {
+            this.driver.element('a.board-name').html(function(title) {
+                title.should.equal(this.params.boardTitle)
+            })
+        })
         .then('the user has the access level (.*)', function(level) {
             var driver = this.driver
             driver.wait(function() {
